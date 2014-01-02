d842cf80fcaa701442d7f4837b5fe0f7cc0600f0
Ruben Gomez <ruben@pixable.com>
2014-01-02 20:13:39 +0100

    Fixed some layout issues

--- gen/com/tweetpaper/R.java
+++ gen/com/tweetpaper/R.java
@@ -8,6 +8,9 @@
 package com.tweetpaper;
 
 public final class R {
+    public static final class array {
+        public static final int intervals=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class drawable {
@@ -16,32 +19,33 @@ public final class R {
         public static final int sign_in_twitter_button=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000c;
-        public static final int hashtag=0x7f080007;
-        public static final int hashtag_edit=0x7f080009;
-        public static final int hashtag_logo=0x7f080008;
-        public static final int interval=0x7f080004;
-        public static final int interval_selector=0x7f080006;
-        public static final int interval_text=0x7f080005;
-        public static final int save_config=0x7f08000a;
-        public static final int settings_screen=0x7f080003;
-        public static final int twitter_login=0x7f080000;
-        public static final int twitter_login_button=0x7f080001;
-        public static final int twitter_login_text=0x7f080002;
-        public static final int webView=0x7f08000b;
+        public static final int action_settings=0x7f09000c;
+        public static final int hashtag=0x7f090007;
+        public static final int hashtag_edit=0x7f090009;
+        public static final int hashtag_logo=0x7f090008;
+        public static final int interval=0x7f090004;
+        public static final int interval_selector=0x7f090006;
+        public static final int interval_text=0x7f090005;
+        public static final int save_config=0x7f09000a;
+        public static final int settings_screen=0x7f090003;
+        public static final int twitter_login=0x7f090000;
+        public static final int twitter_login_button=0x7f090001;
+        public static final int twitter_login_text=0x7f090002;
+        public static final int webView=0x7f09000b;
     }
     public static final class layout {
         public static final int activity_tweetpaper_settings=0x7f030000;
         public static final int activity_webview=0x7f030001;
     }
     public static final class menu {
-        public static final int tweetpaper_settings=0x7f070000;
+        public static final int tweetpaper_settings=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hashtag=0x7f050006;
         public static final int interval_text=0x7f050003;
+        public static final int intervals_prompt=0x7f050007;
         public static final int save_config=0x7f050004;
         public static final int title_activity_tweetpaper_settings=0x7f050001;
         public static final int twitter_login_text=0x7f050005;
@@ -68,11 +72,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
     public static final class xml {
         public static final int wallpaper=0x7f040000;
